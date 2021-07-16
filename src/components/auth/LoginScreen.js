9096450960aa6bcf17a174f5fa3051a1f02e7d2a
ckubotaQ <ckubota@quaxar.com>
2021-07-16 12:30:05 -0500

    init project

---
+++ src/components/auth/LoginScreen.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export const LoginScreen = () => {
+    return (
+        <>
+            <h1>Login</h1>
+            <form>
+                <input type="text" 
+                placeholder="email"
+                name="email"
+                />
+                <input type="password" 
+                placeholder="password"
+                name="password"
+                />
+                <button
+                type="submit"
+
+                >
+                    Login
+                </button>
+                <hr/>
+                Google
+            </form>
+        </>
+    )
+}
